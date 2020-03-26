c2d4d52221ba012d41c04fb7b4dc569b781b0727
Francesco <fra.iozzia@gmail.com>
2020-03-26 13:46:47 +0100

    manca ancora qualcosa

--- main.js
+++ main.js
@@ -3,17 +3,27 @@
 // e per i multipli di 5 stampi “Buzz”.
 // // Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
 
-
+var precedente;
+var simbolo;
 
 for (var i=1; i < 101; i++) {
   if(i % 3 && i % 5){
-    console.log(i);
+    simbolo= i;
+    simbolo="frizzbuzz"
+    precedente=document.getElementById('msg').innerHTML;
+    document.getElementById('msg').innerHTML= precedente + "<li>" + simbolo + "</li>";
   }
   else{
-    if(i % 3== 0)
-    console.log("Fizz");
+    if(i % 3=== 0)
+    simbolo= i;
+    simbolo="friz"
+    precedente=document.getElementById('msg').innerHTML;
+    document.getElementById('msg').innerHTML= precedente + "<li>" + simbolo + "</li>";
   }
-  if(i % 5== 0){
-    console.log("Buzz")
+  if(i % 5=== 0){
+    simbolo= i;
+    simbolo="buzz"
+    precedente=document.getElementById('msg').innerHTML;
+    document.getElementById('msg').innerHTML= precedente + "<li>" + simbolo + "</li>";
   }
 }
